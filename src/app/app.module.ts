ea885e0e49081e33a67f1199436ca27ddf82b5d9
unknown <safitaher01@gmail.com>
2021-12-22 12:02:08 +0100

    Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatMenuModule} from '@angular/material/menu'; 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './HomePage/home/home.component'; 
+import {MatProgressBarModule} from '@angular/material/progress-bar';  
+import {MatGridListModule} from '@angular/material/grid-list'; 
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+//Lotte Player
+export function playerFactory() {
+  return player;
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    LottieModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
